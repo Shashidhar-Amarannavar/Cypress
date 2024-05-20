9aee145369d4030a1960b766a513f107db718d4f
Shashidhar Amarannavar <shashidharamarannavar@gmail.com>
2024-05-20 18:33:20 +0530

    Project set_up

---
+++ cypress/e2e/step_definitions/loginStep.js
@@ -0,0 +1,12 @@
+import{Given, When, Then} from "cypress-cucumber-preprocessor/steps"
+
+Given('user in login screen', async function () {
+    
+});
+When('user enter the email and password', async function () {
+    
+});
+
+Then('Verify popup {string}', async function (expectedText) {
+  
+});
