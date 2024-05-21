d1e5456efe7ed4ab96c0ff441f38a5b02eab1e49
Shashidhar Amarannavar <shashidharamarannavar@gmail.com>
2024-05-21 19:56:31 +0530

    Completed login scenarios

--- cypress/e2e/features/loginScreen/login.feature
+++ cypress/e2e/features/loginScreen/login.feature
@@ -5,3 +5,28 @@ Feature: Verify login screen
     Given user in login screen
     When user enter the email and password
     Then Verify popup "Successfully logged in."
+
+  @loginScreen
+  Scenario Outline: Login to the application
+    Given user in login screen
+    When user enter the "<email>" and "<password>"
+    Then Verify popup "<message>"
+
+    Examples:
+      | email                                 | password    | message                        |
+      | shashidhar.amarannavar@softsuave.org  | Shashi#123  | Successfully logged in.        |
+      | shashidhar.amarannavar@softsuave.org  | Shashi#1231 | Wrong password entered.        |
+      | shashidhar.amarannavar1@softsuave.org | Shashi#123  | No user found. Please sign up. |
+      | shashidhar.amarannavar1@softsuave.org | Shashi#1231 | No user found. Please sign up. |
+
+  @loginScreen @validationEmailAndPassword
+  Scenario Outline: Verify the email and password textbox validation message
+    Given user in login screen
+    When user enter the wrong "<email>" and "<password>"
+    Then Verify the textbox "<validation message>"
+
+    Examples:
+      | email                  | password  | validation message                 |
+      | shashidhar.amarannavar |           | Please enter a valid email address |
+      |                        | Shashi#12 | Please enter your password         |
+      |                        |           | Please enter your email            |
--- cypress/pages/loginPageComponent.js
+++ cypress/pages/loginPageComponent.js
@@ -1,6 +1,8 @@
 
 export default class LoginPageComponents {
 
+    
+
     locators = {
         button: {
             login: "//button[text()='Login']",
@@ -18,19 +20,47 @@ export default class LoginPageComponents {
         }
     }
 
-    async loginToApp(email, password) {
+    fillLoginCredentials(email, password) {
         cy.xpath(this.locators.textField.email).type(email);
         cy.xpath(this.locators.textField.password).type(password)
         cy.xpath(this.locators.button.login).click()
     }
 
-    async loginToApplication() {
-        cy.xpath(this.locators.textField.email).type();
-        cy.xpath(this.locators.textField.password).type()
-        cy.xpath(this.locators.button.login).click()
+    verifyToastMessage(expectedText) {
+        cy.xpath(this.locators.textField.toastMessage).should('have.text', expectedText);
     }
 
-     verifyToastMessage(expectedText) {
-        cy.xpath(this.locators.textField.toastMessage).should('have.text', expectedText);
+    enterEmailAndPassword(email, password) {
+        if (!email && !password) {
+            cy.xpath(this.locators.textField.email).type("passing null value")
+            cy.xpath(this.locators.textField.email).clear();
+            cy.xpath(this.locators.textField.emailValidation).then((text) => {
+                this.actualText = text.text();
+            })
+        } else if (!password) {
+            cy.xpath(this.locators.textField.email).clear();
+            cy.xpath(this.locators.textField.email).type(email);
+            cy.xpath(this.locators.textField.emailValidation).then((text) => {
+                this.actualText = text.text();
+            })
+
+        } else {
+            cy.xpath(this.locators.textField.password).type(password);
+            cy.xpath(this.locators.textField.password).clear();
+            cy.xpath(this.locators.textField.passwordValidation).then((text) => {
+                this.actualText = text.text();
+            })
+        }
+    }
+
+    validateTextMessage(expectedText) {
+        try {
+            // cy.xpath(this.locators.textField.emailValidation).contains(expectedText);
+            expect(this.actualText).contain(expectedText)
+        } catch (error) {
+            // cy.xpath(this.locators.textField.passwordValidation).contains(expectedText);
+            expect(this.actualText).contain(expectedText)
+            // cy.get('#password-helper-text > span').contains(expectedText)
+        }
     }
 }
